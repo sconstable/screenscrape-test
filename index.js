9fe5c095d80c0a2d123e1381a279527bbefbea3b
Stephen Constable <steve.constable@gmail.com>
2020-10-24 15:36:27 -0400

    write inner HTML to a file instead of screenshot

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const puppeteer = require('puppeteer')
+const fs = require('fs')
+
+function extractItems() {
+  const extractedElements = document.querySelectorAll('[id|="nav"]')
+  const items = []
+  for (let element of extractedElements) {
+    items.push(element.innerText)
+  }
+  return items
+}
 
 void (async () => {
   try {
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
     await page.goto('https://scrapethissite.com')
-    await page.screenshot({
-      path: 'page1.png'
-    })
+    const items = await page.evaluate(extractItems);
+    fs.writeFileSync('items.txt', items.join('\n') + '\n')
     await browser.close()
   } catch (error) {
     console.log(error)

e220457ff3e19dc98b2a95aee42138a6662a9511
sconstable <steve.constable@gmail.com>
2020-11-01 10:00:13 -0500

    Scraping twitter.com now

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const puppeteer = require('puppeteer')
 const fs = require('fs')
+const readline = require('readline')
 
 function extractItems() {
-  const extractedElements = document.querySelectorAll('[id|="nav"]')
+  const extractedElements = document.querySelectorAll('[role=article]')
   const items = []
   for (let element of extractedElements) {
     items.push(element.innerText)
@@ -12,12 +13,25 @@ function extractItems() {
 
 void (async () => {
   try {
-    const browser = await puppeteer.launch()
+    const rl = readline.createInterface({
+	    input: process.stdin,
+	    output: process.stdout,
+    })
+    console.log('Which twitter profile should I access?')
+    const it = rl[Symbol.asyncIterator]();
+    const profile = await it.next()
+    const browser = await puppeteer.launch({
+                headless: false,
+    })
     const page = await browser.newPage()
-    await page.goto('https://scrapethissite.com')
+    await page.goto('https://twitter.com/'+profile.value)
+    await page.waitForSelector('[role=article]', {
+      visible: true,
+    })
     const items = await page.evaluate(extractItems);
     fs.writeFileSync('items.txt', items.join('\n') + '\n')
     await browser.close()
+    await rl.close()
   } catch (error) {
     console.log(error)
   }
